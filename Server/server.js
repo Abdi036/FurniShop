ee137251e6c840584c8e8592faa086e9ea589865
Abdi036 <abdikumela036@gmail.com>
2024-10-07 15:46:06 -1200

    making folder and file structure

---
+++ Server/server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const path = require("path");
+const xss = require("xss-clean");
+const mongosanitize = require("express-mongo-sanitize");
+
+// files
+const userRoute = require("./routes/userRoutes");
+const productRoute = require("./routes/productRoutes");
+const paymentRoutes = require("./routes/paymentRoutes");
+
+const errorMiddleware = require("./controllers/errorController");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+// Middlewares
+// NOSQL quiery injection protection
+app.use(mongosanitize());
+app.use(xss());
+
+app.use(cors());
+app.use(express.json());
+
+// Serve static files from the "userImages" directory
+app.use("/userImages", express.static(path.join(__dirname, "userImages")));
+app.use(
+  "/productImages",
+  express.static(path.join(__dirname, "productImages"))
+);
+
+// Database connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("DB connected successfully");
+  })
+  .catch((error) => {
+    console.error("DB connection failed:", error.message);
+  });
+
+// Routes
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/products", productRoute);
+app.use("/api/v1/payment", paymentRoutes);
+
+// Global Error Handling Middleware
+app.use(errorMiddleware);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
