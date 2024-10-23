abdfa00813f8c0728b314bd31c93a312e9b289e6
Abdi036 <abdikumela036@gmail.com>
2024-10-22 14:35:56 -1200

    check

--- Server/server.js
+++ Server/server.js
@@ -53,6 +53,6 @@ app.use("/api/v1/payment", paymentRoutes);
 app.use(errorMiddleware);
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
